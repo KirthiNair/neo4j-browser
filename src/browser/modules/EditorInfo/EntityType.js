dd5edf89ef910dc43c6a0a0e4519decd10192770
Kirthi Nair <kirthi.nair@synerzip.com>
2019-05-15 17:04:20 +0530

    Updated JSDoc for entityType

--- src/browser/modules/EditorInfo/EntityType.js
+++ src/browser/modules/EditorInfo/EntityType.js
@@ -5,6 +5,10 @@ import {
   DrawerSectionBody
 } from 'browser-components/drawer'
 
+/**
+ *
+ * This functional component is used to display the type of entity on selection.
+ */
 export const EntityType = ({ itemType = '' }) => {
   return (
     <DrawerSection>
