2f251a5735979708b3b76aa24561392bf38f9726
Kirthi Nair <kirthi.nair@synerzip.com>
2019-05-15 18:00:12 +0530

    Resolved conflict

--- src/browser/modules/EditorInfo/NodeLabel.js
+++ src/browser/modules/EditorInfo/NodeLabel.js
@@ -1,12 +1,13 @@
+/**
+ * This component is used to display the node label on editor drawer
+ */
 import React from 'react'
 import {
   DrawerSubHeader,
   DrawerSection,
   DrawerSectionBody
 } from 'browser-components/drawer'
-/**
- * This component is used to display the node label on editor drawer
- */
+
 export const NodeLabel = ({ nodeLabel = '' }) => {
   return nodeLabel ? (
     <DrawerSection>
